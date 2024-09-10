3fb59053daf87ffb2422c1106849f6cfff381585
MrJaiKure <kurejaideep2003@gmail.com>
2024-09-10 14:29:49 +0530

    new

--- Backend/index.js
+++ Backend/index.js
@@ -1,1 +1,2 @@
 console.log("tushar")
+console.log("jaideep");
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,10 @@
-
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div >
-
-
-<h1 className="text-3xl font-bold underline">
-      Hello project
-    </h1>
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello project</h1>
+      <div className="text-red-800 font-bold">this is Jaideep</div>
     </div>
   );
 }
