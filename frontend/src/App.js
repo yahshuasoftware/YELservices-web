fbd472b9967dba74476546196be9fe51880b561c
MrJaiKure <kurejaideep2003@gmail.com>
2024-09-10 14:33:22 +0530

    new

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ function App() {
     <div>
       <h1 className="text-3xl font-bold underline">Hello project</h1>
       <div className="text-red-800 font-bold">this is Jaideep</div>
+      <div>this is new one</div>
     </div>
   );
 }
