3baccb061b125c6dce1d387e6c5ce26c364f0498
MrJaiKure <kurejaideep2003@gmail.com>
2024-09-14 22:13:59 +0530

    services and login/register page working succesfully

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,23 @@
-<<<<<<< HEAD
-import "./App.css";
-<<<<<<< HEAD
-import ServicesAvailable from "./Component/Services/ServicesAvailable";
 
-function App() {
-  return (
-    <div>
-      <ServicesAvailable/>
-    </div>
-=======
-=======
+import "./App.css";
+import ServicesAvailable from "./Component/Services/ServicesAvailable";
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import AppRoutes from './routes/AppRoutes'; // Import the routes
->>>>>>> tushar
 
 function App() {
   return (
+  <>  
+    <div>
+      <ServicesAvailable/>
+      </div>
+
     <Router>
       <AppRoutes /> {/* Render the routes here */}
     </Router>
->>>>>>> 34168262b3940812b45c7064d66f43aed55c7dc9
+    </>
+
+    
   );
 }
 
