5a2ed5d8357a5eed9bf94413d65b7e6e79cb2216
tushar <tusharsarode1830@gmail.com>
2024-10-15 07:18:40 -0700

    change in index.js port

--- Backend/index.js
+++ Backend/index.js
@@ -9,20 +9,20 @@ app.use(express.static("uploads"));
 
 const connectDB=require("./config/DB")
 
-const Port = process.env.REACT_APP_PORT;
-const port = Port;
+
 
 connectDB()
 // Middleware to parse JSON bodies 
 app.use(express.json());
 app.use(cors({
     origin: ['http://100.26.236.115:3000','http://localhost:3000'],
-   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
 }));
 
 app.use("/app",router);
 
 
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+const PORT = process.env.REACT_APP_PORT || 8000  ;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
