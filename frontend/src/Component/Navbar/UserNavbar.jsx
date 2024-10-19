d0d14b54954dda078340995a5e73052fa2011e7f
MrJaiKure <kurejaideep2003@gmail.com>
2024-10-19 13:30:44 +0530

    log

--- frontend/src/Component/Navbar/UserNavbar.jsx
+++ frontend/src/Component/Navbar/UserNavbar.jsx
@@ -80,24 +80,28 @@ const UserNavbar = () => {
   if (error) return <div>Error: {error}</div>;
   return (
     <div>
-      <div className="justify-between">
-        <ul className="pl-10 pr-10 grid md:grid-cols-8 grid-cols-4 lg:grid-cols-8 bg-gray-300">
-          <li className="hover:bg-orange-700 hover:text-white flex items-center justify-center pt-4 pb-4 font-thin text-xs h-16">
+      <div className="justify-between  bg-gray-300 flex">
+        <ul className="pl-10 pr-10 grid md:grid-cols-3 ml-16 grid-cols-4 lg:grid-cols-3">
+          <li className="hover:bg-orange-700 ml-2 mr-2 hover:text-white flex items-center justify-center p-4 font-normal text-xs h-16">
             <span className="m-1"><IoMdHome /></span>Home
           </li>
         
-          <li className="hover:bg-orange-700 hover:text-white flex items-center justify-center pt-4 pb-4 font-thin text-xs h-16">
+          <li className="hover:bg-orange-700 ml-2 mr-2 hover:text-white flex items-center justify-center p-4 font-normal text-xs h-16">
             <span className="m-1"><FaUser /></span>{user.name}
           </li>
           <Link to="/userdashboard">
-            <li className="hover:bg-orange-700 hover:text-white flex items-center justify-center pt-4 pb-4 font-thin text-xs h-16">
+            <li className="hover:bg-orange-700 ml-2 mr-2 hover:text-white flex items-center justify-center p-4 font-normal text-xs h-16">
               <span className="m-1"><FaTachometerAlt /></span>DASHBOARD
             </li>
           </Link>
-          <button onClick={handleLogout} className="bg-red-800 text-white py-2 px-4 rounded hover:bg-black" >logout</button>
 
         </ul>
+        <div className="flex justify-end">
+          <button onClick={handleLogout} className="bg-red-800 mr-3 text-white py-2 px-4 rounded hover:bg-black" >logout</button>
+          </div>
       </div>
+     
+
     </div>
   );
 };
