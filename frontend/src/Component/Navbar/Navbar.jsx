74158701995697f5897b935dfb5ac3f705ec4957
tushar <tusharsarode1830@gmail.com>
2024-10-26 07:11:01 -0700

    home page ui

--- frontend/src/Component/Navbar/Navbar.jsx
+++ frontend/src/Component/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { FaChild, FaUser, FaBriefcase, FaPhoneAlt, FaTachometerAlt, FaBars } from "react-icons/fa";
 import { IoMdHome } from "react-icons/io";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -11,33 +12,36 @@ const Navbar = () => {
 
   return (
     <div className="relative">
-      {/* Hamburger Icon */}
+      {/* Hamburger Icon */}.
       <div className="md:hidden p-2" onClick={toggleMenu}>
         <FaBars className="text-xl cursor-pointer" />
       </div>
 
+
       {/* Full Navbar for larger screens and mobile screens */}
       <div className={`md:block ${isOpen ? "block" : "hidden"} text-sm sm:text-xs md:text-base lg:text-lg`}>
+      
+
         <ul className="pl-1 pr-1 grid grid-cols-1 md:grid-cols-8 bg-gray-300">
-          <li className="hover:bg-orange-700 hover:text-white flex items-center justify-start pt-1 pb-1 font-thin text-xs md:text-sm lg:text-base lg:mx-7">
+          <Link to="/" className="hover:bg-orange-700 hover:text-white flex items-center justify-start pt-1 pb-1 font-thin text-xs md:text-sm lg:text-base lg:mx-7">
             <span className="m-1"><IoMdHome /></span> Home
-          </li>
+          </Link>
          
-          <li className="hover:bg-orange-700 hover:text-white flex items-center justify-start pt-1 pb-1 font-thin text-xs md:text-sm lg:text-base">
+          <Link className="hover:bg-orange-700 hover:text-white flex items-center justify-start pt-1 pb-1 font-thin text-xs md:text-sm lg:text-base">
             <span className="m-1"><FaBriefcase /></span> SERVICES INFORMATION
-          </li>
-          <li className="hover:bg-orange-700 hover:text-white flex items-center justify-start pt-1 pb-1 font-thin text-xs md:text-sm lg:text-base">
+          </Link>
+          <Link to="/contactus" className="hover:bg-orange-700 hover:text-white flex items-center justify-start pt-1 pb-1 font-thin text-xs md:text-sm lg:text-base">
             <span className="m-1"><FaPhoneAlt /></span> CONTACT US
-          </li>
-          <li className="hover:bg-orange-700 hover:text-white flex items-center justify-start pt-1 pb-1 font-thin text-xs md:text-sm lg:text-base">
+          </Link>
+          <Link to="/sevakendra" className="hover:bg-orange-700 hover:text-white flex items-center justify-start pt-1 pb-1 font-thin text-xs md:text-sm lg:text-base">
             <span className="m-1"><FaUser /></span> SEWA KENDRA
-          </li>
-          <li className="hover:bg-orange-700 hover:text-white  flex items-center justify-start pt-1 pb-1 font-thin text-xs md:text-sm lg:text-base">
+          </Link>
+          <Link to="dashboard" className="hover:bg-orange-700 hover:text-white  flex items-center justify-start pt-1 pb-1 font-thin text-xs md:text-sm lg:text-base">
             <span className="m-1"><FaTachometerAlt /></span> DASHBOARD
-          </li>
-          <li className="hover:bg-orange-700 hover:text-white  flex items-center justify-start pt-1 pb-1 font-thin text-xs md:text-sm lg:text-base">
+          </Link>
+          <Link to="/careers" className="hover:bg-orange-700 hover:text-white  flex items-center justify-start pt-1 pb-1 font-thin text-xs md:text-sm lg:text-base">
             <span className="m-1"><FaTachometerAlt /></span> CAREERS
-          </li>
+          </Link>
         </ul>
       </div>
     </div>
--- frontend/src/routes/Home.js
+++ frontend/src/routes/Home.js
@@ -10,8 +10,8 @@ const Home = () => {
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
-      <div className="flex-grow sm:flex-col px-5  h-auto lg:h-[90vh] bg-[url('../public/images/image.jpeg')] bg-cover bg-center gap-2 grid grid-cols-1 lg:grid-cols-2">
-        <div>
+      <div className="flex-grow items-center justify-center sm:flex-col px-10  h-auto lg:h-[90vh] bg-[url('../public/images/image.jpeg')] bg-cover bg-center gap-2 grid grid-cols-1 lg:grid-cols-2">
+        <div >
           <ServicesAvailable />
         </div>
         <div>
